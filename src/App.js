22a37b0a6b349a2420fae2a16b6f680571c46b97
Hamad Kashmiri kashmirh@uni.coventry.ac.uk <kashmirh@coventry.ac.uk>
2021-04-25 18:08:59 +0000

    styled added and responsive 2nd component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -10,6 +11,7 @@ class App extends Component {
       return (
         <div>
           <Navbar />
+        <br />
           <Footer />
         </div>
   
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Footer() {
   return (
-    <div className="footer-primary"> 
+   <footerStyle className="footer-primary"> 
+    <div className="footer-main">
       <div className="container"> 
         <div className="row">
           <div className="col-md-4 col-sm-6">
-            <h4> Footer </h4>
+            <h4> Footer 1</h4>
             <ul className="list-unstyled">
               <li>dummy, 1</li>
               <li>dummy, 1</li>
@@ -14,7 +16,7 @@ export default function Footer() {
             </ul>
           </div>
           <div className="col-md-4 col-sm-6">
-            <h4> Footer </h4>
+            <h4> Footer 2</h4>
             <ul className="list-unstyled">
               <li>dummy, 1</li>
               <li>dummy, 1</li>
@@ -22,7 +24,7 @@ export default function Footer() {
             </ul>
           </div>
           <div className="col-md-4 col-sm-6">
-            <h4> Footer </h4>
+            <h4> Footer 3</h4>
             <ul className="list-unstyled">
               <li>dummy, 1</li>
               <li>dummy, 1</li>
@@ -35,5 +37,14 @@ export default function Footer() {
         </div>
       </div>
     </div>
+   </footerStyle>
   )
 }
+
+const footerStyle = styled.footer`
+  .footer-main {
+    background: var(--mainBlack);
+    padding-top: 3rem;
+color: var(main)
+    }
+`;
