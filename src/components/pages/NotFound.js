7055ecc0e55854f2f4e1f8bcf397e9d1c73ca9ce
Hamad Kashmiri kashmirh@uni.coventry.ac.uk <kashmirh@coventry.ac.uk>
2021-04-26 18:26:51 +0000

    completed http error pages

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,27 @@
+//page inspired from 80/20 coding youtube channel
 import React, {Component} from 'react';
 
 class NotFound extends Component {
   render() {
     return (
-      <div> NotFound </div>
+      <div className="container" style = {{ marginTop: '100px'}}>
+        <div className="row">
+          <div className="col-md-12">
+             <div className="error-template">
+                <h1 className="display-4 font-weight-bold">Oops!</h1>
+                <h2 className="display-7">404 Error - Not Found!</h2>
+                <div className="error-details">
+                      There was an error on the server. The page you are trying to access was not found!
+                </div>
+                <div>
+                  <a href='/'>
+                    <button className="btn btn-lg" style = {{ background:'#F2AA4CFF', color: '#fff', marginTop: '20px'}}>Go Home</button>
+                  </a>
+                </div>
+             </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/pages/Unauthorized.js
+++ src/components/pages/Unauthorized.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 class Unauthorized extends Component {
   render() {
     return (
-      <div> Unauthorized </div>
+      <div className="container" style = {{ marginTop: '100px'}}>
+        <div className="row">
+          <div className="col-md-12">
+             <div className="error-template">
+                <h1 className="display-4 font-weight-bold">Oops!</h1>
+                <h2 className="display-7">401 Error - unauthorized!</h2>
+                <div className="error-details">
+                      There was an error on the server. You do not have permission to access this resource!
+                </div>
+                <div>
+                  <a href='/'>
+                    <button className="btn btn-lg" style = {{ background:'#F2AA4CFF', color: '#fff', marginTop: '20px'}}>Go Home</button>
+                  </a>
+                </div>
+             </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
