d0a960f75baa612c278224191d8d70c0e17b5bb4
Hamad Kashmiri kashmirh@uni.coventry.ac.uk <kashmirh@coventry.ac.uk>
2021-04-26 00:34:50 +0000

    state added to context with dummy data

---
+++ src/appdata.js
@@ -0,0 +1,54 @@
+export const dogData = [{
+  _id: 1,
+  name: "Rodney",
+  breed: "Pitbull",
+  traits: ["Loveable", "Fast"],
+  descripton: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est",
+  imageURL: "https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559__340.jpg"
+  age: 6,
+  date: "date",
+  shelterID: "shelter"
+  
+},
+                          {
+  _id: 2,
+  name: "Rodney",
+  breed: "Pitbull",
+  Traits: ["Loveable", "Fast"],
+  descripton: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est",
+  imageURL: "https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559__340.jpg"
+  age: 6,
+  date: "date",
+  shelterID: 2
+  
+},
+                          {
+  _id: 3,
+  name: "Rodney",
+  breed: "Pitbull",
+  Traits: ["Loveable", "Fast"],
+  descripton: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est",
+  imageURL: "https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559__340.jpg"
+  age: 6,
+  date: "date",
+  shelterID: 3
+  
+}];
+
+export const shelterData = [{
+  
+  name: "London Shelter",
+  address: "12 pedestrian road",
+  contactNo: 10
+  
+},
+{
+  name: "London2 Shelter",
+  address: "13 pedestrian road",
+  contactNo: 10
+  
+}]
+
+export favouriteData = [{
+  name: "favourite1"
+}]
--- src/components/Context.js
+++ src/components/Context.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-
+import { dogData, shelterData, favouriteData } from '../appdata'
 const DataContext = React.createContext();
 //provider gets data
 //consumer consumes and uses that data
 
 class DataProvider extends Component {
+  
+  state = {
+    dogs: dogData,
+    shelters: shelterData,
+    favourites: favouriteData
+  }
+  
   render() {
     return (
-      <DataContext.Provider value="data provider value">
+      <DataContext.Provider value={
+      dogs: this.state.dogs,
+      shelters: this.state.dogs,
+      favourites: this.state.favourites}>
       { this.props.children }
       </DataContext.Provider>
     )
